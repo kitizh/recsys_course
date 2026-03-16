2e9c9fdf8e1199e339b80fc25f3722f3efc09c0d
ohglazano <ohglazano@gmail.com>
2026-03-16 19:29:34 +0300

    sem1 done

--- sem1.py
+++ sem1.py
@@ -1,8 +1,8 @@
 """
-Seminar 1 
+Seminar 1
 Построение простой рекомендательной системы на основе популярности товаров
 
-Цель: Разработать простейшие рекомендательные системы, 
+Цель: Разработать простейшие рекомендательные системы,
 на основе имеющихся данных о взаимодействии с фильмами,
 проанализировать их эффективность.
 """
@@ -25,11 +25,13 @@ def random_recommend(n_recommendations: int = 10, seed: int = 42) -> list[int]:
     """
     ratings_df, _ = load_data()
     np.random.seed(seed)
-    recommendations = []    
+    recommendations = []
 
     ### Ваш код здесь ###
-    recommendations = ratings_df['movieId'].to_numpy()
-    recommendations = np.random.choice(recommendations, size=n_recommendations)
+    recommendations = ratings_df["movieId"].unique()
+    recommendations = np.random.choice(
+        recommendations, size=n_recommendations, replace=False
+    )
     recommendations = recommendations.tolist()
     ### Конец вашего кода ###
 
@@ -53,6 +55,26 @@ def top_n_recommend(
     top_n_recs = []
 
     ### Ваш код здесь ###
+    movie_stats = (
+        ratings_df.groupby("movieId")["rating"]
+        .agg(["mean", "count"])  # средняя оценка и общее число оценок
+        .reset_index()
+    )  # получится таблица вида movieId | mean | count
+
+    # оставляем фильмы с количеством рейтингов выше минимального
+    movie_stats = movie_stats[movie_stats["count"] >= min_ratings]
+
+    # сортируем по убыванию
+    movie_stats = movie_stats.sort_values(
+        by=["mean", "count"], ascending=[False, False]
+    )
+
+    # брем верхние n_recommendations записей и каждую запись записываем как кортеж
+    top_n_recs = list(
+        movie_stats.head(n_recommendations)[["movieId", "mean", "count"]].itertuples(
+            index=False, name=None
+        )
+    )
 
     ### Конец вашего кода ###
 
@@ -79,6 +101,28 @@ def evaluate_rec_systems(
     popular_accuracy = 0.0
     ### Ваш код здесь ###
 
+    # список фильмов которые пользователь оценивал
+    user_movies = set(ratings_df[ratings_df["userId"] == user_id]["movieId"])
+
+    # список id случайных n рекомендаций
+    random_recs_ids = random_recommend(
+        n_recommendations=n_recommendations, seed=random_state
+    )
+
+    # список n популярных рекомендаций
+    popular_recs_with_stats = top_n_recommend(n_recommendations=n_recommendations)
+
+    # оставляем только id популярных рекомендаций
+    popular_recs_ids = [m_id for m_id, _, _ in popular_recs_with_stats]
+
+    # берем пересечение
+    random_hits = len(set(random_recs_ids) & user_movies)
+    popular_hits = len(set(popular_recs_ids) & user_movies)
+
+    # делим число совпадений на общее число рекомендаций
+    random_accuracy = random_hits / n_recommendations
+    popular_accuracy = popular_hits / n_recommendations
+
     ### Конец вашего кода ###
     return {"random_accuracy": random_accuracy, "popular_accuracy": popular_accuracy}
 
@@ -94,14 +138,10 @@ if __name__ == "__main__":
     print("\n2. ПОПУЛЯРНЫЕ ФИЛЬМЫ (рекомендации на основе популярности):")
     print("-" * 60)
     popular_recs = top_n_recommend(n_recommendations=10)
-    print(
-        f"{'Rank':<5} {'ID':<6} {'Ср рейтинг':<18} {'Кол-во оценок':<15} {'Название'}"
-    )
+    print(f"{'Rank':<5} {'ID':<6} {'Ср рейтинг':<18} {'Кол-во оценок':<15}")
     print("-" * 60)
-    for i, (movie_id, avg_rating, rating_count, title) in enumerate(popular_recs, 1):
-        print(
-            f"{i:<5} {movie_id:<6} {avg_rating:<18.2f} {rating_count:<15} {title[:50]}"
-        )
+    for i, (movie_id, avg_rating, rating_count) in enumerate(popular_recs, 1):
+        print(f"{i:<5} {movie_id:<6} {avg_rating:<18.2f} {rating_count:<15}")
 
     # 3. Оценка системы
     print("\n3. ОЦЕНКА КАЧЕСТВА СИСТЕМЫ:")
